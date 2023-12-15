b640fc785158d9a6ed3f5262aad1d750c3f02239
Narmina Khrushchova <namira.lir@gmail.com>
2023-12-15 18:09:23 +0300

    Добавление двум функциям докстринг

--- str_func.py
+++ str_func.py
@@ -1,4 +1,21 @@
 def second_func(value):
+    """
+
+    Функции для заглавных букв
+    """
+    list_val = value.split()
+    new_list = []
+    for val in list_val:
+        new_val = val.capitalize()
+        new_list.append(new_val)
+
+    return ' '.join(new_list)
+
+
+def second_func(value):
+    """
+    Функция для всех заглавных букв
+    """
     list_val = value.split()
     new_list = []
     for val in list_val:

65a27af6a81c059028f74e3c1a701a717973c92e
Narmina Khrushchova <namira.lir@gmail.com>
2023-12-15 17:28:38 +0300

    Добавление файла str_func

---
+++ str_func.py
@@ -0,0 +1,9 @@
+def second_func(value):
+    list_val = value.split()
+    new_list = []
+    for val in list_val:
+        new_val = val.capitalize()
+        new_list.append(new_val)
+
+    return ' '.join(new_list)
+
